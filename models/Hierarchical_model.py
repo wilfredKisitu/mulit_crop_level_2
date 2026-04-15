e45787ca10c5f33f13931a56e5f95af5498908dc
wilfredKisitu <wktdcourses@gmail.com>
2026-04-15 20:43:20 +0300

    updated the pipeline for hierarchical model training

--- models/Hierarchical_model.py
+++ models/Hierarchical_model.py
@@ -1,4 +1,3 @@
-import torch
 import torch.nn as nn
 from models.disease_heads import DiseaseHeads
 
@@ -11,15 +10,55 @@ class HierarchicalModel(nn.Module):
         self.disease_heads= DiseaseHeads(512, disease_class_per_plant)
 
         self.plant_idx_to_name = {
-            0: "cassava",
-            1: "apple",
-            2: "banana",
-            3: "tomato",
-            4: "maize"
+            0: "apple",
+            1: "bellpepper",
+            2: "cherry",
+            3: "corn",
+            4: "grape",
+            5: "peach",
+            6: "potato",
+            7: "strawberry",
+            8: "tomato"
         }
 
     def forward(self, x):
         features = self.backbone(x)
         plant_logits = self.plant_head(features)
         return plant_logits, features
+
+
+if __name__ == "__main__":
+    print(f"Testing the hierarchical model")
+    import torch
+    from Dataset.dataset_obj import PlantDataset
+    from configs.project_dirs import TRAIN_PATH
+    from models.Resent_model import Resnet
+
+    dataset = PlantDataset(TRAIN_PATH)
+    crops = dataset.crop_types
+    num_plants = len(dataset.crop_types)
+    num_diseases = len(dataset.disease_types)
+    disease_class_per_plant = dataset.get_disease_per_crop_count()
+
+    backbone = Resnet(num_plants, num_diseases, return_features=True)
+
+    model = HierarchicalModel(
+        backbone=backbone, 
+        num_plantts=num_plants, 
+        disease_class_per_plant=disease_class_per_plant
+    )
+
+    model.eval()
+    print(f'Model initailized successfully')
+
+    batch_size = 4
+    x = torch.randn(batch_size, 3, 224, 224)
+    plant_logits, features = model(x)
+
+    feature_dim = 512
+
+    print(f'Features shaped:  {features.shape}, expected shape: ({batch_size}, {feature_dim})')
+    print(f'Plant logits shape: {plant_logits.shape}, expected shape: ({batch_size}, {num_plants})')
     
+
+    print(f'Supported crops: \n {crops}')
--- models/disease_heads.py
+++ models/disease_heads.py
@@ -13,11 +13,15 @@ class DiseaseHeads(nn.Module):
         super().__init__()
 
         self.heads = nn.ModuleDict({
-            "cassava": nn.Linear(feature_dim, disease_classes_per_plant['cassava']),
             "apple": nn.Linear(feature_dim, disease_classes_per_plant["apple"]),
-            "banana": nn.Linear(feature_dim, disease_classes_per_plant["banana"]),
-            "tomato": nn.Linear(feature_dim, disease_classes_per_plant["tomato"]),
-            "maize": nn.Linear(feature_dim, disease_classes_per_plant["maize"])
+            "bellpepper": nn.Linear(feature_dim, disease_classes_per_plant["bellpepper"]),
+            "cherry": nn.Linear(feature_dim, disease_classes_per_plant["cherry"]),
+            "corn": nn.Linear(feature_dim, disease_classes_per_plant["corn"]),
+            "grape": nn.Linear(feature_dim, disease_classes_per_plant["grape"]),
+            "peach": nn.Linear(feature_dim, disease_classes_per_plant["peach"]),
+            "potato": nn.Linear(feature_dim, disease_classes_per_plant["potato"]),
+            "strawberry": nn.Linear(feature_dim, disease_classes_per_plant["strawberry"]),
+            "tomato": nn.Linear(feature_dim, disease_classes_per_plant["tomato"])
         })
     
     def forward(self, features, plant_name):
@@ -26,3 +30,29 @@ class DiseaseHeads(nn.Module):
 
 if __name__ == "__main__":
     """Implement the test case of the following model"""
+    import torch
+    from Dataset.dataset_obj import PlantDataset
+    from configs.project_dirs import TRAIN_PATH
+   
+
+    dataset = PlantDataset(TRAIN_PATH)
+    crops = dataset.crop_types
+    disease_per_crop_count = dataset.get_disease_per_crop_count()
+    feature_dim = 512
+    model = DiseaseHeads(feature_dim, disease_per_crop_count)
+
+    model.eval()
+    batch_size = 8
+    features = torch.randn(batch_size, feature_dim)
+
+    for plant, num_classes in disease_per_crop_count.items():
+        out = model(features, plant)
+
+        print(f'\nTesting plant: {plant}')
+        print(f'Expected output shape: ({batch_size}, {num_classes})')
+        print(f'Actual shape output: {tuple(out.shape)}')
+    
+
+    print(f'Crop types supported for adjustment of heads:\n {crops}')
+    print(f'Disease per crop type: \n {disease_per_crop_count}')
+    
